e1309f72ecea75447c05f5d4d76d35e560cc410b
dujia <dujia@iti-inc.co.jp>
2020-12-08 16:58:13 +0900

    Initial Commit

---
+++ MultiDownloadDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MultiDownloadDemo
+//
+//  Created by dujia on 2020/12/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
